cd482dc58c75cab3a9c85fbc1083d37d7dfd00ef
2ndPrince <astonisher88@gmail.com>
2019-10-08 13:31:58 -0400

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    martinTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [{
   'id': 1,
   'image': 'https://placeimg.com/64/64/1',
-  'name': 'Hong Gil Dong1',
+  'name': 'Hong Gil Dong',
   'birthday': '880405',
   'gender': 'male',
   'job': 'college student'
@@ -27,12 +45,29 @@ const customers = [{
   'job': 'General'
 }]
 
-function App() {
-  return (
-    <div>
-      { customers.map(c=> { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> )}) }
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>IMAGE</TableCell>
+              <TableCell>NAME</TableCell>
+              <TableCell>BIRTHDAY</TableCell>
+              <TableCell>GENDER</TableCell>
+              <TableCell>JOB</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          { customers.map(c=> { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> )}) }
+          </TableBody>
+        </Table>
+      </Paper> 
+    );
+  } 
 }
 
-export default App;
+export default withStyles(styles)(App);
