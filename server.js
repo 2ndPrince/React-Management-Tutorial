716e86968856bbbc57dba166490b716d8b259ad8
2ndPrince <astonisher88@gmail.com>
2019-10-09 14:34:59 -0400

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,35 +7,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/hello', (req, res) => {
     res.send({message: 'Hello Express!'});
 });
 
 app.get('/api/customers', (req, res) => {
-    res.send([{
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': 'HongGil Dong',
-        'birthday': '880405',
-        'gender': 'male',
-        'job': 'college student'
-      },
-      {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': 'Sejong Daewang',
-        'birthday': '890405',
-        'gender': 'male',
-        'job': 'King'
-      },
-      {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': 'Sunsin Lee',
-        'birthday': '900405',
-        'gender': 'female',
-        'job': 'General'
-      }]
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, field) => {
+        res.send(rows);
+      }
     );
 });
 
