4eef2477be5d0b2859c1f706ba6e86cb142b199f
2ndPrince <astonisher88@gmail.com>
2019-10-09 20:07:47 -0400

    Add Material UI Design for Add & Delete

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class CustomerDelete extends React.Component {
     
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            open: false
+        })
+    }
+
     deleteCustomer(id){
         // /api/customers/7
         const url = '/api/customers/' + id;
@@ -13,7 +38,26 @@ class CustomerDelete extends React.Component {
     
     render(){
         return(
+            <div>
+                <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>
+                    DELETE
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle>DELETE CAUTION</DialogTitle>
+                    <DialogContent>
+                        <Typography gutterBottom>
+                            Selected Customer will be deleted
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={(e) => {this.deleteCustomer(this.props.id)}}>DELETE</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>CANCEL</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+            /*
             <button onClick={(e) => {this.deleteCustomer(this.props.id)}}>DELETE</button>
+            */
         )
     }
 }
